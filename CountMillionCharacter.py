ba867c50931c91f20d028b9a4190409a5cc2e80a
raquelsalmeron <65223144+raquelsalmeron@users.noreply.github.com>
2020-05-24 11:41:08 +1000

    Update CountMillionCharacter.py
    added a message

--- CountMillionCharacter.py
+++ CountMillionCharacter.py
@@ -310,3 +310,5 @@ print("String\n {} \n".format(wordstring))
 print("List\n {} \n".format(str(wordlist)))
 print("Frequencies\n {} \n".format(str(wordfreq)))
 print("Pairs\n {}".format(str(dict(zip(wordlist, wordfreq)))))
+
+print("Adding my two cents here")
